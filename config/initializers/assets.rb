cddbc3f25fee04ec1ec8d0333be9224e517638b1
Houndsto0th <bry.kinnear@gmail.com>
2014-12-14 00:41:20 -0700

    Fonts working locally, clearly Heroku is a bitch

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
 Rails.application.config.assets.precompile += %w( jqBootstrapValidation.js )
 Rails.application.config.assets.precompile += %w( contact_me.js )
 Rails.application.config.assets.precompile += %w( agency.js )
+  

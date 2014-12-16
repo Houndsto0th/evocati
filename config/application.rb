e5b95beb92f8843b54ee2191b478c414d56a60da
Houndsto0th <bry.kinnear@gmail.com>
2014-12-16 16:53:14 -0700

    added new layout and stream bin

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Vindicatum
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
 
     config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)$/
     # Settings in config/environments/* take precedence over those specified here.

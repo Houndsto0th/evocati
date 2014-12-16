aed7f84646d2771c013747d05a7529a880fd213e
Houndsto0th <bry.kinnear@gmail.com>
2014-12-16 15:12:45 -0700

    adding better stream information display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,45 +1,37 @@
 class PagesController < ApplicationController
+  MEMBERS = [
+    #Tanks
+    {name:"Houndstooth", role: "Tank", class: "DK", warlogs: "2932"},
+    {name:"Neurf", role: "Tank", class: "Warrior", warlogs: "925912"},
+    {name:"Twobeers", role: "Tank", class: "Monk", warlogs: "2930"},
+    #Healers
+    {name: "Jurik", role: "Healer", class: "Druid", warlogs: "2936"},
+    {name: "Espressomonk", role: "Healer", class: "Monk", warlogs: "671312"},
+    {name: "Farore", role: "Healer", class: "Shaman", warlogs: "941326"},
+    {name: "Gimpybroseph", role: "Healer", class: "Priest", warlogs: "540780"},
+    {name: "Ihz", role: "Healer", class: "Paladin", warlogs: "326466"},
+    #DPS
+    {name: "Arrow", role: "DPS", class: "Hunter", warlogs: "807868"},
+    {name: "Cis", role: "DPS", class: "Warlock", warlogs: "2928"},
+    {name: "Hafbar", role: "DPS", class: "Hunter", warlogs: "574705"},
+    {name: "Ilzan", role: "DPS", class: "Rogue", warlogs: "87885"},
+    {name: "Ippon", role: "DPS", class: "Rogue", warlogs: "326467"},
+    {name: "Keirimath", role: "DPS", class: "Paladin", warlogs: "2935"},
+    {name: "Kinoble", role: "DPS", class: "Druid", warlogs: "4714"},
+    {name: "Luminoire", role: "DPS", class: "Priest", warlogs: "236300"},
+    {name: "Paleaura", role: "DPS", class: "DK", warlogs: "204050"},
+    {name: "Powerwaddle", role: "DPS", class: "Paladin", warlogs: "509481"},
+    {name: "Titandust", role: "DPS", class: "Warrior", warlogs: "972145"},
+  ]
+
   def index
-    @members = [
-          #Tanks
-          {name:"Houndstooth", role: "Tank", class: "DK", warlogs: "2932"},
-          {name:"Neurf", role: "Tank", class: "Warrior", warlogs: "925912"},
-          {name:"Twobeers", role: "Tank", class: "Monk", warlogs: "2930"},
-          #Healers
-          {name: "Jurik", role: "Healer", class: "Druid", warlogs: "2936"},
-          {name: "Espressomonk", role: "Healer", class: "Monk", warlogs: "671312"},
-          {name: "Farore", role: "Healer", class: "Shaman", warlogs: "941326"},
-          {name: "Gimpybroseph", role: "Healer", class: "Priest", warlogs: "540780"},
-          {name: "Ihz", role: "Healer", class: "Paladin", warlogs: "326466"},
-          #DPS
-          {name: "Arrow", role: "DPS", class: "Hunter", warlogs: "807868"},
-          {name: "Cis", role: "DPS", class: "Warlock", warlogs: "2928"},
-          {name: "Hafbar", role: "DPS", class: "Hunter", warlogs: "574705"},
-          {name: "Ilzan", role: "DPS", class: "Rogue", warlogs: "87885"},
-          {name: "Ippon", role: "DPS", class: "Rogue", warlogs: "326467"},
-          {name: "Keirimath", role: "DPS", class: "Paladin", warlogs: "2935"},
-          {name: "Kinoble", role: "DPS", class: "Druid", warlogs: "4714"},
-          {name: "Luminoire", role: "DPS", class: "Priest", warlogs: "236300"},
-          {name: "Paleaura", role: "DPS", class: "DK", warlogs: "204050"},
-          {name: "Powerwaddle", role: "DPS", class: "Paladin", warlogs: "509481"},
-          {name: "Titandust", role: "DPS", class: "Warrior", warlogs: "972145"},
-    ]
+    @members = MEMBERS
+    @armory_responses = WowAPI.new.get_characters(MEMBERS)
 
-    hydra = Typhoeus::Hydra.new
-    requests = @members.map {|member|
-      request = Typhoeus::Request.new(
-      "http://us.battle.net/api/wow/character/Icecrown/#{member[:name]}",
-      headers: {Accept: "application/json"},
-      )
-      hydra.queue(request)
-      request
-    }
-    hydra.run
+    #faraday request
+    # @members.each do |member|
+    # @armory_responses = WowAPI.new.get_character("member[:name]")
 
-    @armory_responses = requests.map { |request|
-      JSON.parse(request.response.body)
-
-    }
 
   end
 
--- app/models/wow_api.rb
+++ app/models/wow_api.rb
@@ -1,26 +1,41 @@
 class WowAPI
-  # def initialize
-  #   @conn = Faraday.new(url: 'http://us.battle.net')
-  # end
-  #
-  # def get_character(character)
-  #   response = @conn.get do |req|
-  #     req.url "/api/wow/character/Icecrown/#{character}"
-  #     req.headers['Content-Type'] = 'application/json'
-  #
-  #   end
-  #
-  # JSON.parse(response.body, symbolize_names: true)
-  # end
 
-  # def get_character(character, hydra)
-  #   request = Typhoeus::Request.new(
-  #     "http://us.battle.net/api/wow/chracter/Icecrown/#{character}",
-  #     headers: {Accept: "application/json"},
-  #   )
-  #   hydra.queue(request)
-  #   request
-  #
-  # end
+  def initialize(hydra = Typhoeus::Hydra.new)
+    @hydra = hydra
+  end
+
+  def get_characters(members)
+    responses = []
+    members.each do |member|
+      request = Typhoeus::Request.new(
+        "http://us.battle.net/api/wow/character/Icecrown/#{member[:name]}",
+        headers: {Accept: "application/json"},
+      )
+      request.on_complete { |response| responses << JSON.parse(request.response.body) }
+      @hydra.queue(request)
+    end
+    @hydra.run
+    responses
+  end
 
 end
+
+
+
+
+
+# Faraday request to compare to Typhoeus
+# def initialize
+#   @conn = Faraday.new(url: 'http://us.battle.net')
+# end
+#
+# def get_character(character)
+#   response = @conn.get do |req|
+#     req.url "/api/wow/character/Icecrown/#{character}"
+#     req.headers['Content-Type'] = 'application/json'
+#
+#   end
+#
+# JSON.parse(response.body, symbolize_names: true)
+# end
+#end

bd5c90ee3e8de5a3e2c7f33e359a29d2e9bd3dcd
Houndsto0th <bry.kinnear@gmail.com>
2015-01-11 22:27:36 -0700

    added new killshot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,7 +33,10 @@ $(document).ready(function(){
         $("header").removeClass("kill2").addClass("kill3")
       }
       else if($("header").hasClass("kill3")) {
-        $("header").removeClass("kill3").addClass("kill1")
+        $("header").removeClass("kill3").addClass("kill4")
+      }
+      else if($("header").hasClass("kill4")) {
+        $("header").removeClass("kill4").addClass("kill1")
       }
 
     }

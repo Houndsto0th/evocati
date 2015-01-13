4611b8aeb3dcce1f3d55b6d57b4947e91f89680c
Houndsto0th <bry.kinnear@gmail.com>
2015-01-13 14:21:50 -0700

    updated roster to have officers/members

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,33 @@
 class PagesController < ApplicationController
+  OFFICERS = [
+    {name: "Tanned - GM", role: "DPS", class: "Priest", warlogs: "2929", rank: "GM"},
+    {name:"Houndstooth", role: "Tank", class: "DK", warlogs: "2932", rank: "Officer"},
+    {name:"Neurf", role: "Tank", class: "Warrior", warlogs: "925912", rank: "Officer"},
+    {name:"Twobeers", role: "Tank", class: "Monk", warlogs: "2930", rank: "Officer"},
+    {name: "Sepa", role: "Healer", class: "Paladin", warlogs: "9432", rank: "Officer"},
+    {name: "Jurik", role: "Healer", class: "Druid", warlogs: "2936", rank: "Officer"},
+  ]
+
   MEMBERS = [
-    #Tanks
-    {name:"Houndstooth", role: "Tank", class: "DK", warlogs: "2932"},
-    {name:"Neurf", role: "Tank", class: "Warrior", warlogs: "925912"},
-    {name:"Twobeers", role: "Tank", class: "Monk", warlogs: "2930"},
     #Healers
-    {name: "Jurik", role: "Healer", class: "Druid", warlogs: "2936"},
-    {name: "Farore", role: "Healer", class: "Shaman", warlogs: "941326"},
-    {name: "Gimpybroseph", role: "Healer", class: "Priest", warlogs: "540780"},
-    {name: "Sepa", role: "Healer", class: "Paladin", warlogs: "9432"},
+    {name: "Farore", role: "Healer", class: "Shaman", warlogs: "941326", rank: "Member"},
+    {name: "Gimpybroseph", role: "Healer", class: "Priest", warlogs: "540780", rank: "Member"},
+    {name: "Luminoire", role: "Healer", class: "Priest", warlogs: "236300", rank: "Member"},
 
     #DPS
-    {name: "Arrow", role: "DPS", class: "Hunter", warlogs: "807868"},
-    {name: "Tanned", role: "DPS", class: "Priest", warlogs: "2929"},
-    {name: "Cis", role: "DPS", class: "Warlock", warlogs: "2928"},
-    {name: "Ilzan", role: "DPS", class: "Rogue", warlogs: "87885"},
-    {name: "Ippon", role: "DPS", class: "Rogue", warlogs: "326467"},
-    {name: "Keirimath", role: "DPS", class: "Paladin", warlogs: "2935"},
-    {name: "Kinoble", role: "DPS", class: "Druid", warlogs: "4714"},
-    {name: "Luminoire", role: "DPS", class: "Priest", warlogs: "236300"},
-    {name: "Paleaura", role: "DPS", class: "DK", warlogs: "204050"},
-    {name: "Powerwaddle", role: "DPS", class: "Paladin", warlogs: "509481"},
-    {name: "Titandust", role: "DPS", class: "Warrior", warlogs: "972145"},
+    {name: "Arrow", role: "DPS", class: "Hunter", warlogs: "807868", rank: "Member"},
+    {name: "Cis", role: "DPS", class: "Warlock", warlogs: "2928", rank: "Member"},
+    {name: "Ilzan", role: "DPS", class: "Rogue", warlogs: "87885", rank: "Member"},
+    {name: "Ippon", role: "DPS", class: "Rogue", warlogs: "326467", rank: "Member"},
+    {name: "Keirimath", role: "DPS", class: "Paladin", warlogs: "2935", rank: "Member"},
+    {name: "Kinoble", role: "DPS", class: "Druid", warlogs: "4714", rank: "Member"},
+    {name: "Paleaura", role: "DPS", class: "DK", warlogs: "204050", rank: "Member"},
+    {name: "Powerwaddle", role: "DPS", class: "Paladin", warlogs: "509481", rank: "Member"},
+    {name: "Titandust", role: "DPS", class: "Warrior", warlogs: "972145", rank: "Member"},
   ]
 
   def index
+    @officers = OFFICERS
     @members = MEMBERS
     # @armory_responses = WowAPI.new.get_characters(MEMBERS)
 

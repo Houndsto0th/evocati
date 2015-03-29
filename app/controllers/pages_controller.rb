435c3ef859f09a81ecc951353402cb9c8ce665e1
Houndsto0th <bry.kinnear@gmail.com>
2015-03-29 16:02:00 -0600

    added roster update and new images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,32 +1,32 @@
 class PagesController < ApplicationController
   TANKS = [
-    {name: "Marrcus", spec: "Blood", class: "DK", warlogs: "658789", realm: "malygos"},
+    {name: "Sorahka", spec: "Brewmaster", class: "Monk", warlogs: "658789", realm: "icecrown"},
     {name: "Dizeased", spec: "Blood", class: "DK", warlogs: "2410527", realm: "icecrown"}
   ]
 
   HEALERS = [
     {name: "Iberia", spec: "Holy", class: "Paladin", warlogs: "571964", realm: "icecrown"},
-    {name: "Kimchii", spec: "Mistwalker", class: "Monk", warlogs: "118570", realm: "icecrown"},
-    {name: "Ssen", spec: "Restoration", class: "Druid", warlogs: "5735834", realm: "icecrown"},
+    {name: "Alexiistexas", spec: "Restoration", class: "Shaman", warlogs: "118570", realm: "icecrown"},
     {name: "Cerinez", spec: "Restoration", class: "Druid", warlogs: "6230057", realm: "icecrown"},
     {name: "Cuppycakez", spec: "Discipline", class: "Priest", warlogs: "996352", realm: "icecrown"}
 
   ]
 
   DPS = [
-    {name: "Fuzzycuffsqt", spec: "Survival", class: "Hunter", warlogs: "2104073", realm: "icecrown"},
-    {name: "Sauma", spec: "Marksman", class: "Hunter", warlogs: "1015266", realm: "malygos"},
-    {name: "Katharla", spec: "Survival", class: "Hunter", warlogs: "425855", realm: "icecrown"},
     {name: "Bec", spec: "Combat", class: "Rogue", warlogs: "941149", realm: "malygos"},
-    {name: "Jibrone", spec: "Arcane", class: "Mage", warlogs: "996346", realm: "icecrown"},
     {name: "Aryel", spec: "Frost", class: "Mage", warlogs: "781013", realm: "malygos"},
-    {name: "Ogspokes", spec: "Retribution", class: "Paladin", warlogs: "2530", realm: "icecrown"},
-    {name: "Nakedwarrior", spec: "Fury", class: "Warrior", warlogs: "326615", realm: "icecrown"},
+    {name: "Ssen", spec: "Balance", class: "Druid", warlogs: "996346", realm: "icecrown"},
+    {name: "Marrcus", spec: "Unholy", class: "DK", warlogs: "996346", realm: "malygos"},
     {name: "Slumber", spec: "Fury", class: "Warrior", warlogs: "887307", realm: "icecrown"},
     {name: "Zeblen", spec: "Enhancement", class: "Shaman", warlogs: "6355612", realm: "icecrown"},
-    {name: "Alisu", spec: "Feral", class: "Druid", warlogs: "2529", realm: "icecrown"},
+    {name: "Sauma", spec: "Marksman", class: "Hunter", warlogs: "1015266", realm: "malygos"},
     {name: "Oomtree", spec: "Balance", class: "Druid", warlogs: "441772", realm: "icecrown"},
+    {name: "Ogspokes", spec: "Retribution", class: "Paladin", warlogs: "2530", realm: "icecrown"},
+    {name: "Katharla", spec: "Survival", class: "Hunter", warlogs: "425855", realm: "icecrown"},
+    {name: "Alisu", spec: "Feral", class: "Druid", warlogs: "2529", realm: "icecrown"},
+    {name: "Fuzzycuffsqt", spec: "Survival", class: "Hunter", warlogs: "2104073", realm: "icecrown"},
     {name: "Drynin", spec: "Balance", class: "Druid", warlogs: "712247", realm: "icecrown"},
+    {name: "Caspeco", spec: "Enhancement", class: "Shaman", warlogs: "326615", realm: "icecrown"},
   ]
 
   def index
